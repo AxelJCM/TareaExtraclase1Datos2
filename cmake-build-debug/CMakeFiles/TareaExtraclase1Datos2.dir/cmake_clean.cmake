80fa597f966c67772a5d1b314aa288570f9fca90
Axel <axeljcm@estudiantec.cr>
2021-08-19 22:13:28 -0400

    Initialization c++

---
+++ cmake-build-debug/CMakeFiles/TareaExtraclase1Datos2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TareaExtraclase1Datos2.dir/main.cpp.o"
+  "TareaExtraclase1Datos2"
+  "TareaExtraclase1Datos2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaExtraclase1Datos2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
